797e2d5e8a995e28152abc37bc6a68a5485ebfdc
andreszapataq <andreszapataq@gmail.com>
2025-01-26 14:15:21 -0500

    update page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "andreszapata.me - Welcome",
+  title: "andreszapata.me - Welcome to ^Z",
   description: "^Z Portolio",
 };
 
